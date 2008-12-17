3d43e04ccd182cec4f5f1455c31deb45f69b42ab
Leon Berenschot <leonb@beriedata.nl>
2008-12-17 14:37:15 +0100

    mobile en desktop browsers krijgen andere content

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,34 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-end
+  exempt_from_layout('iphone_html.erb')  
+    
+  before_filter :adjust_format_for_iphone  
+    
+  protected  
+  def iphone_user_agent?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+  end
+
+  def adjust_format_for_iphone    
+    request.format = :iphone if iphone_user_agent?
+  end
+    
+  # def check_iphone  
+  #   if iphone_user_agent?  
+  #     request.parameters[:format] = 'iphone_html'  
+  #   end  
+  # end  
+end  
+  
+# class DashboardController < ApplicationController  
+#   def index  
+#     @top_movies = Movie.top_movies  
+#     @movie = @top_movies.first  
+#       
+#     respond_to do |format|  
+#       format.html # index.html.erb  
+#       format.iphone_html #index.iphone_html.erb  
+#     end  
+#   end  
+# end  
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
-  layout "iphone"
+  # layout "iphone"
   def index
+    @page_title = t('location.title')
     @locations = Location.find(:all, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC")#, :limit => '50'.reverse
     # 
     # Create a new map object, also defining the div ("map") 
@@ -24,47 +25,56 @@ class LocationsController < ApplicationController
     end
     last_location = Variable.new("last_location")
 
-    # Maak alle icons voor accuracy
-    @accuracy_icons = {
-      0 => @map.icon_global_init(GIcon.new(:image => "/images/accuracy_icon_0.png",
-         :icon_anchor => GPoint.new(15,15),
-         :info_window_anchor => GPoint.new(15,15)), "img_route_point_0",
-         :icon_size => GSize.new(30,30))      
-    }
-    10.step(100,10) { |point|
-      @map.icon_global_init(GIcon.new(:image => "/images/acc_icon_#{point}.png",
-         :icon_anchor => GPoint.new(point/2,point/2),
-         :info_window_anchor => GPoint.new(point/2,point/2)), "img_route_point_#{point}",
-         :iconSize => GSize.new(point,point))
-      @accuracy_icons[point] = Variable.new("img_route_point_#{point}")
-    }
-
-    @route_points = []
-    markers1 = []
-
-    # alle meetpunten definieren
-    @locations.each { |loc|
-      @route_points << [loc.latitude, loc.longitude]
-      markers1 << GMarker.new([loc.latitude, loc.longitude], 
-          :icon => get_accuracy_icon(loc.horizontal_accuracy.to_i))
-    }
-    # laat registratiepunten allee nzien op level 16-17
-    managed_markers1 = ManagedMarker.new(markers1,16,17)
-
     kop_optocht_info = "Kop Optocht"
     kop_optocht = GMarker.new([@locations.first.latitude, @locations.first.longitude], 
           :icon => last_location )#,
           # :title => 'Deventer', 
           # :info_window => "#{kop_optocht_info}")
 
-    route = GPolyline.new(@route_points,"#ff0000",5,1.0)
-    route_2 = GPolyline.new(@route_points,"#ffcc00",3,0.8)
-          
-    mm = GMarkerManager.new(@map,:managed_markers => [managed_markers1])
-    @map.declare_init(mm, "mgr")
-    @map.overlay_init(route)
-    @map.overlay_init(route_2)
+
     @map.overlay_init(kop_optocht)
+    
+    respond_to do |format|  
+      format.html do # index.html.erb  
+        # Maak alle icons voor accuracy
+        @accuracy_icons = {
+          0 => @map.icon_global_init(GIcon.new(:image => "/images/accuracy_icon_0.png",
+             :icon_anchor => GPoint.new(15,15),
+             :info_window_anchor => GPoint.new(15,15)), "img_route_point_0",
+             :icon_size => GSize.new(30,30))      
+        }
+        10.step(100,10) { |point|
+          @map.icon_global_init(GIcon.new(:image => "/images/acc_icon_#{point}.png",
+             :icon_anchor => GPoint.new(point/2,point/2),
+             :info_window_anchor => GPoint.new(point/2,point/2)), "img_route_point_#{point}",
+             :iconSize => GSize.new(point,point))
+          @accuracy_icons[point] = Variable.new("img_route_point_#{point}")
+        }
+
+        @route_points = []
+        markers1 = []
+
+        # alle meetpunten definieren
+        @locations.each { |loc|
+          @route_points << [loc.latitude, loc.longitude]
+          markers1 << GMarker.new([loc.latitude, loc.longitude], 
+              :icon => get_accuracy_icon(loc.horizontal_accuracy.to_i))
+        }
+        # laat registratiepunten allee nzien op level 16-17
+        managed_markers1 = ManagedMarker.new(markers1,16,17)
+
+        route = GPolyline.new(@route_points,"#ff0000",5,1.0)
+        route_2 = GPolyline.new(@route_points,"#ffcc00",3,0.8)
+          
+        mm = GMarkerManager.new(@map,:managed_markers => [managed_markers1])
+        @map.declare_init(mm, "mgr")
+        @map.overlay_init(route)
+        @map.overlay_init(route_2)
+        
+      end
+      format.iphone  # index.iphone.erb
+    end  
+
   end
   
   def new
