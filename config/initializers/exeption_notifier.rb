942c9d1715af3ce2a4e4b37f45d5558289b0a840
Leon Berenschot <leonb@beriedata.nl>
2009-01-24 13:33:57 +0100

    other email adres for exeptionnotifier (google rejects local ISP)

--- config/initializers/exeption_notifier.rb
+++ config/initializers/exeption_notifier.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-ExceptionNotifier.exception_recipients = %w(leipeleon@gmail.com)
+ExceptionNotifier.exception_recipients = %w(leonb@beriedata.nl)
 ExceptionNotifier.sender_address = %("Leon Berenschot" <leonb@beriedata.nl>)
 ExceptionNotifier.email_prefix = "[OC Kop] "
