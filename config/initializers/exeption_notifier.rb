6a5e9088b1cedcbd198cfce9972588d1ea2b71f7
Leon Berenschot <leonb@beriedata.nl>
2009-01-24 12:49:23 +0100

    include ExceptionNotifiable

---
+++ config/initializers/exeption_notifier.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
+ExceptionNotifier.exception_recipients = %w(leipeleon@gmail.com)
+ExceptionNotifier.sender_address = %("Leon Berenschot" <leonb@beriedata.nl>)
+ExceptionNotifier.email_prefix = "[OC Kop] "
