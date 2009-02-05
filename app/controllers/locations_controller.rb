53c284858e0db09b86e47fb6678e315af920c204
Leon Berenschot <leonb@beriedata.nl>
2009-02-05 19:18:21 +0100

    Refactor locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,9 +4,11 @@ class LocationsController < ApplicationController
   # layout "iphone"
   def index
     @page.title = t('location.title')
-    @locations = Location.find(:all, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC", :limit => 10 )#, :limit => '50'.reverse
+    # @locations = Location.find(:all, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC", :limit => 10 )#, :limit => '50'.reverse
+    @location = Location.last
+    @route = Route.find(:all)
 
-    if @locations.size > 0
+    if @location
       get_map
       set_zoom
       get_head
@@ -17,22 +19,25 @@ class LocationsController < ApplicationController
     respond_to do |format|
       format.iphone
       format.html do
-        get_traveled if @locations.size > 0
+        get_route
+        # get_traveled if @location.size > 0
       end
       format.js do
-        get_traveled if @locations.size > 0
+        get_route
+        # get_traveled if @location.size > 0
       end
     end
   end
 
   def last_locations
     @map = Variable.new("map")
-    # @map = GMap.new("map")
 
-    # params[:last_update]
-    @locations = Location.find(:all, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC", :limit => 10 )#, :limit => '50'.reverse
+    @location = Location.last
+    @route = Route.find(:all)
+    
     head  = get_head('add')
-    route = get_traveled('add')
+    # traveled = get_traveled('add')
+    route = get_route('add')
     center = set_zoom('add')
 
     respond_to do |format|
@@ -41,6 +46,7 @@ class LocationsController < ApplicationController
           page << @map.clear_overlays
           page << head
           page << route
+          # page << traveled
           page << center
         end
         # get_accuracy
@@ -50,13 +56,12 @@ class LocationsController < ApplicationController
 
   def just_map
     @page.title = t('location.title')
-    @locations = Location.find(:all, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC", :limit => 10 )#, :limit => '50'.reverse
+    @location = Location.last
 
     get_map(false,false)
     set_zoom('init', 16)
     get_head
-    get_traveled
-
+    get_route
   end
 
   def new
@@ -102,7 +107,7 @@ private
   end
 
   def get_accuracy
-    @accuracy = "<p>Precisie: +/- <%= @locations.first.horizontal_accuracy.to_i %> meter, <%= distance_of_time_in_dutch(@locations.first.created_at, Time.now, true) %> geleden</p>"
+    @accuracy = "<p>Precisie: +/- <%= @location.horizontal_accuracy.to_i %> meter, <%= distance_of_time_in_dutch(@location.created_at, Time.now, true) %> geleden</p>"
   end
 
   def authenticate
@@ -123,7 +128,7 @@ private
   def get_head(type= 'init')
     # create the icon : "/images/knob_big.png"
     last_location = Variable.new("last_location")
-    if @locations.first.horizontal_accuracy.to_i < 100
+    if @location.horizontal_accuracy.to_i < 100
       @map.icon_global_init(GIcon.new(:image => "/images/knob_small.png",
          :icon_anchor => GPoint.new(78/2,78/2),
          :info_window_anchor => GPoint.new(78/2,78/2)), "last_location")
@@ -134,7 +139,7 @@ private
     end
 
     kop_optocht_info = "Kop Optocht"
-    kop_optocht = GMarker.new([@locations.first.latitude, @locations.first.longitude], 
+    kop_optocht = GMarker.new([@location.latitude, @location.longitude], 
           :icon => last_location )
 
     case type 
@@ -144,20 +149,86 @@ private
     end
   end
 
-  def set_zoom(type = 'init', level = 17) # Center the map on specific coordinates and focus in fairly closely
+  def set_zoom(type = 'init', level = 14) # Center the map on specific coordinates and focus in fairly closely
     case type 
-    when 'add':  @map.set_center(GLatLng.new([@locations.first.latitude, @locations.first.longitude]))
-    else         @map.center_zoom_init([@locations.first.latitude, @locations.first.longitude], level)
+    when 'add':  @map.set_center(GLatLng.new([@location.latitude, @location.longitude]))
+    else         @map.center_zoom_init([@location.latitude, @location.longitude], level)
     end
   end
   
   def get_traveled(type = 'init')
-    # Maak alle icons voor accuracy
+    @traveled_points = []
+    markers1 = []
+
+    create_accuracy_icons
+
+    # alle meetpunten definieren
+    @location.each { |loc|
+      @traveled_points << [loc.latitude, loc.longitude]
+      markers1 << GMarker.new([loc.latitude, loc.longitude], 
+          :icon => get_accuracy_icon(loc.horizontal_accuracy.to_i))
+    }
+    # laat registratiepunten allee nzien op level 16-17
+    managed_markers1 = ManagedMarker.new(markers1,16,17)
+
+    traveled = GPolyline.new(@traveled_points,"#ff0000",5,1.0)
+    traveled_2 = GPolyline.new(@traveled_points,"#ffcc00",3,0.8)
+      
+    mm = GMarkerManager.new(@map,:managed_markers => [managed_markers1])
+
+    if type == 'init' 
+      [@map.overlay_init(traveled), @map.overlay_init(traveled_2)]
+    else         
+      [@map.add_overlay(traveled), @map.add_overlay(traveled_2)]
+    end
+  end
+
+  def get_markers(type = 'init')
+    markers1 = []
+    create_accuracy_icons
+    @route.each { |loc|
+      markers1 << GMarker.new([loc.latitude, loc.longitude], 
+          :icon => get_accuracy_icon(10))
+    }
+    # laat registratiepunten alleen zien op level 16-17
+    managed_markers1 = ManagedMarker.new(markers1,16,17)
+    mm = GMarkerManager.new(@map,:managed_markers => [managed_markers1])
+
+    if type == 'init' 
+      @map.declare_init(mm, "mgr")
+    else         
+      @map.declare(mm, "mgr")
+    end
+  end
+
+  def get_route(type = 'init')
+    @route_points = []
+
+    create_accuracy_icons
+
+    # alle meetpunten definieren
+    @route.each { |loc|
+      @route_points << [loc.latitude, loc.longitude]
+    }
+
+    route = GPolyline.new(@route_points,"#ff0000",5,1.0)
+    route_2 = GPolyline.new(@route_points,"#ffcc00",3,0.8)
+      
+    if type == 'init' 
+      [@map.overlay_init(route), @map.overlay_init(route_2)]
+    else
+      [@map.add_overlay(route), @map.add_overlay(route_2)]
+    end
+  end
+
+  # Maak alle icons voor accuracy
+  def create_accuracy_icons
+    return @accuracy_icons if @accuracy_icons
     @accuracy_icons = {
       0 => @map.icon_global_init(GIcon.new(:image => "/images/accuracy_icon_0.png",
          :icon_anchor => GPoint.new(15,15),
          :info_window_anchor => GPoint.new(15,15)), "img_route_point_0",
-         :icon_size => GSize.new(30,30))      
+         :icon_size => GSize.new(30,30))
     }
     10.step(100,10) { |point|
       @map.icon_global_init(GIcon.new(:image => "/images/acc_icon_#{point}.png",
@@ -166,35 +237,7 @@ private
          :iconSize => GSize.new(point,point))
       @accuracy_icons[point] = Variable.new("img_route_point_#{point}")
     }
-
-    @route_points = []
-    markers1 = []
-
-    # alle meetpunten definieren
-    @locations.each { |loc|
-      @route_points << [loc.latitude, loc.longitude]
-      markers1 << GMarker.new([loc.latitude, loc.longitude], 
-          :icon => get_accuracy_icon(loc.horizontal_accuracy.to_i))
-    }
-    # laat registratiepunten allee nzien op level 16-17
-    managed_markers1 = ManagedMarker.new(markers1,16,17)
-
-    route = GPolyline.new(@route_points,"#ff0000",5,1.0)
-    route_2 = GPolyline.new(@route_points,"#ffcc00",3,0.8)
-      
-    mm = GMarkerManager.new(@map,:managed_markers => [managed_markers1])
-    ret = Array.new()
-    case type 
-    when 'add':  
-      # ret << @map.declare(mm, "mgr")
-      ret << @map.add_overlay(route)
-      ret << @map.add_overlay(route_2)
-    else         
-      # ret << @map.declare_init(mm, "mgr")
-      ret << @map.overlay_init(route)
-      ret << @map.overlay_init(route_2)
-    end
-    ret
   end
+
 end
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ActiveRecord::Base
+  
+  def self.last
+    find(:first, :conditions => "horizontal_accuracy < 80", :order => "created_at DESC")#, :limit => 10, :limit => '50'.reverse
+  end
 end
