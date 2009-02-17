f2f8b9bae001cbd9ec5324d4c1706cc4d3834b8a
Leon Berenschot <leonb@beriedata.nl>
2009-02-17 15:35:18 +0100

    translation homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
 
   def index
+    @page_title = t('home.home')
     render
   end
   
   def about
+    @page_title = t('home.about')
     render
   end
 end
