269fa53786b6e5c83334b995952f262b9a3b393c
Leon Berenschot <leonb@beriedata.nl>
2009-02-17 19:42:01 +0100

    Feed lookup in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-
+  before_filter :get_feed
   def index
     @page_title = t('home.home')
     render
@@ -8,5 +8,10 @@ class HomeController < ApplicationController
   def about
     @page_title = t('home.about')
     render
+  end
+
+private
+  def get_feed
+    @feed = Feed.last(3)
   end
 end
