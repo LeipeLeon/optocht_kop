5ef6d1c7f887ab3859dc851d7bc2726e3795a763
Leon Berenschot <leonb@beriedata.nl>
2009-02-17 23:56:02 +0100

    Added Google Analytics for Staging & Production trhu helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,14 +32,14 @@ module ApplicationHelper
   end
   
   def google_analytics
-    if RAILS_ENV == 'production'
+    if GOOGLE_KEYS['analytics']
       '<script type="text/javascript">
       var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
       document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
       </script>
       <script type="text/javascript">
       try {
-      var pageTracker = _gat._getTracker("UA-2394915-5");
+      var pageTracker = _gat._getTracker("' + GOOGLE_KEYS['analytics'] + '");
       pageTracker._trackPageview();
       } catch(err) {}</script>'
     end
