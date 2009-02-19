eed6b438fbf88224ee592996f99c6958bae84459
Leon Berenschot <leonb@beriedata.nl>
2009-02-19 13:41:55 +0100

    respond_to in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,18 @@ class HomeController < ApplicationController
   before_filter :get_feed
   def index
     @page_title = t('home.home')
-    render
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile # index.mobile.erb
+    end
   end
   
   def about
     @page_title = t('home.about')
-    render
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile # index.mobile.erb
+    end
   end
 
 private
