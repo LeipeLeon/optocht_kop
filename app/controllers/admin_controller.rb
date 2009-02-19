b6df1614578d35989f664e179550d8c5370f2195
Leon Berenschot <leonb@beriedata.nl>
2009-02-19 13:58:14 +0100

    respond_to in admin_controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,10 @@ class AdminController < ApplicationController
   before_filter :menu
 
   def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile # index.mobile.erb
+    end
   end
 
 private 
