8db51ab0cc4f7b50c365fa7f92ae9f26f0a0f3b4
Leon Berenschot <leonb@beriedata.nl>
2009-02-19 13:57:38 +0100

    respond_to in twitter_controller

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,8 @@
 class TwitterController < ApplicationController
   def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile # index.mobile.erb
+    end
   end
 end
