a023741a68e6b1b8b0fca5318a929eecff518192
Leon Berenschot <leonb@beriedata.nl>
2009-02-21 10:33:08 +0100

    Cleaning up dir

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090205150543) do
+ActiveRecord::Schema.define(:version => 20090205182041) do
 
   create_table "comatose_page_versions", :force => true do |t|
     t.integer  "comatose_page_id"
