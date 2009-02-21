bba51e7737d751e402a0c1dd0f6615dc68bf7890
Leon Berenschot <leonb@beriedata.nl>
2009-02-21 11:10:05 +0100

    Updated Admin::LocationController to support mobile

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -8,6 +8,7 @@ class Admin::LocationsController < AdminController
 
     respond_to do |format|
       format.html # index.html.erb
+      format.mobile # index.mobile.erb
       format.xml  { render :xml => @locations }
     end
   end
@@ -19,6 +20,7 @@ class Admin::LocationsController < AdminController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.mobile # index.mobile.erb
       format.xml  { render :xml => @locations }
     end
   end
@@ -30,6 +32,7 @@ class Admin::LocationsController < AdminController
 
     respond_to do |format|
       format.html # new.html.erb
+      format.mobile # index.mobile.erb
       format.xml  { render :xml => @locations }
     end
   end
@@ -48,9 +51,11 @@ class Admin::LocationsController < AdminController
       if @locations.save
         flash[:notice] = 'Locations was successfully created.'
         format.html { redirect_to(admin_locations_url) }
+        format.mobile { redirect_to(admin_locations_url) }
         format.xml  { render :xml => @locations, :status => :created, :location => @locations }
       else
         format.html { render :action => "new" }
+        format.mobile { render :action => "new" }
         format.xml  { render :xml => @locations.errors, :status => :unprocessable_entity }
       end
     end
@@ -65,9 +70,11 @@ class Admin::LocationsController < AdminController
       if @locations.update_attributes(params[:location])
         flash[:notice] = 'Locations was successfully updated.'
         format.html { redirect_to(admin_locations_url) }
+        format.mobile { redirect_to(admin_locations_url) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
+        format.mobile { render :action => "edit" }
         format.xml  { render :xml => @locations.errors, :status => :unprocessable_entity }
       end
     end
@@ -81,13 +88,14 @@ class Admin::LocationsController < AdminController
 
     respond_to do |format|
       format.html { redirect_to(admin_locations_url) }
+      format.mobile { redirect_to(admin_locations_url) }
       format.xml  { head :ok }
     end
   end
 
 private
   def set_page_title
-    @page_title = t('deelnemer.title')
+    @page_title = t('location.title')
   end
 
 end
