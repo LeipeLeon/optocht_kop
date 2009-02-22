d8ed1e0871cdb8b04f13a047f4f84db03b1893f7
Leon Berenschot <leonb@beriedata.nl>
2009-02-22 14:27:31 +0100

    Sorting on minifeed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,6 +2,6 @@ class Feed < ActiveRecord::Base
   twitterify :url, :title
   
   def self.last(count = 5)
-    Feed.find(:all, :limit => count)
+    Feed.find(:all, :limit => count, :sort => "created_at DESC")
   end
 end
